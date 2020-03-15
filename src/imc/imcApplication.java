16762a89048217a5bce540de8ec81f8dc44cb4f4
Leonardo de Souza <leosouza1392@gmail.com>
2020-03-15 11:09:29 -0300

    Added Components in Interface

--- src/imc/imcApplication.java
+++ src/imc/imcApplication.java
@@ -1,8 +1,10 @@
 package imc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -28,6 +30,31 @@ public class imcApplication extends JFrame{
         setLocationRelativeTo(this);
         setResizable(false);       
         
-    }
+        /*JLabel Weight*/
+        JLabel lblWeight = new JLabel("Informe seu peso");
+        lblWeight.setBounds(5,5, 160, 32 );
+        contentPane.add(lblWeight);
+        
+        /*JTextField Weight*/
+        JTextField txtWeight = new JTextField();
+        txtWeight.setBounds(5, 30, 160, 32);
+        contentPane.add(txtWeight);
+        
+        /*JLabel Height*/
+        JLabel lblHeight = new JLabel("Informe sua altura");
+        lblHeight.setBounds(5, 60, 160, 32);
+        contentPane.add(lblHeight);
+        
+        /*JTextField Height*/
+        JTextField txtHeight = new JTextField();
+        txtHeight.setBounds(5, 85, 160, 32);
+        contentPane.add(txtHeight);
+        
+        /*JButton Calc*/
+        JButton btnCalc = new JButton("Calcular");
+        btnCalc.setBounds(5, 120, 160, 32);
+        contentPane.add(btnCalc);
+        
+        }
     
 }
